112170eedb1911fef404211fc871a2457637f1d2
Joseph Signorelli <jms26@illinois.edu>
2026-01-21 12:02:02 -0600

    InitializeAcousticField for apps

--- apps/src/common.cpp
+++ apps/src/common.cpp
@@ -1,5 +1,14 @@
 #include "common.hpp"
 
+#include <cmath>
+
+using namespace jabber;
+
+// Helper type for the std::visit
+// (https://en.cppreference.com/w/cpp/utility/variant/visit)
+template<class... Ts>
+struct overloads : Ts... { using Ts::operator()...; };
+
 namespace jabber_app
 {
 
@@ -20,5 +29,68 @@ void PrintBanner(std::ostream &out)
 
    out << banner << std::endl << std::endl;
 }
+AcousticField InitializeAcousticField(const ConfigInput &conf, 
+                                       const std::vector<double> &coords,
+                                       int dim)
+{
+   // Get relevant input metadata
+   const BaseFlowParams &base_conf = conf.BaseFlow();
+   const SourceParamsVariant &source_conf = conf.Source();
+
+   // Initialize acoustic field
+   AcousticField field(dim, coords, base_conf.p, base_conf.rho,
+                        base_conf.U, base_conf.gamma);
+
+      // Assemble vector of wave structs based on input source
+   std::vector<Wave> all_waves;
+
+   std::visit(
+   overloads
+   {
+   [&](const SourceParams<SourceOption::SingleWave> &params_wave)
+   {
+      bool slow = (params_wave.speed == SpeedOption::Slow ? true : false);
+      std::vector<double> k_hat(dim, 0.0);
+      k_hat[0] = std::cos(params_wave.angle);
+      if (dim > 1)
+      {
+         k_hat[1] = std::sin(params_wave.angle);
+      }
+      all_waves.emplace_back(params_wave.amp, params_wave.freq, 
+                            params_wave.phase*M_PI/180.0, slow, k_hat);
+   },
+   [&](const SourceParams<SourceOption::WaveSpectrum> &params_waves)
+   {
+      for (int i = 0; i < params_waves.amps.size(); i++)
+      {
+         bool slow = (params_waves.speeds[i] == SpeedOption::Slow ? true : false);
+         std::vector<double> k_hat(dim, 0.0);
+         k_hat[0] = std::cos(params_waves.angles[i]);
+         if (dim > 1)
+         {
+            k_hat[1] = std::sin(params_waves.angles[i]);
+         }
+         all_waves.emplace_back(params_waves.amps[i], params_waves.freqs[i],
+                              params_waves.phases[i]*M_PI/180.0, slow, k_hat);
+      }
+   }
+   }, source_conf);
+
+   // Apply transfer function
+   // TODO
+
+
+   for (const Wave &wave : all_waves)
+   {  
+      // Add wave to acoustic field
+      field.AddWave(wave);
+   }
+
+   // Finalize the acoustic field initialization
+   field.Finalize();
+
+   return std::move(field);
+
+}                                        
 
 } // namespace jabber_app
--- apps/src/common.hpp
+++ apps/src/common.hpp
@@ -1,13 +1,29 @@
 #ifndef JABBER_APP_COMMON
 #define JABBER_APP_COMMON
 
+#include "config_input.hpp"
+
+#include <jabber.hpp>
 #include <iostream>
 
 namespace jabber_app
 {
 
-/// Print Jabber banner
+/// Print Jabber banner.
 void PrintBanner(std::ostream &out);
+
+/**
+ * @brief Initialize a \ref jabber::AcousticField object from user input and 
+ * grid.
+ * 
+ * @param conf          Input config object.
+ * @param coords        Mesh coordinates vector, in XYZ XYZ ordering.
+ * @param dim           Spatial dimension of mesh.
+ * @return jabber::AcousticField    Finalized acoustic field.
+ */
+jabber::AcousticField InitializeAcousticField(const ConfigInput &conf, 
+                                             const std::vector<double> &coords,
+                                             int dim);
 
 } // namespace jabber_app
 
