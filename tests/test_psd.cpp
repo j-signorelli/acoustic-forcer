3e4c8eaf599d93d677ef160dd61d8bef501c3a63
Joseph Signorelli <jms26@illinois.edu>
2026-02-06 17:11:16 -0600

    Implement segment unit tests for PWLogLogPSD

--- tests/test_psd.cpp
+++ tests/test_psd.cpp
@@ -89,7 +89,7 @@ TEST_CASE("PWLinearPSD integration", "[PSD]")
 
       PWLinearPSD psd(kX,kY);
       CAPTURE(kA, kB);
-      CHECK(psd.Integrate(kA, kB) == kExact);
+      REQUIRE(psd.Integrate(kA, kB) == kExact);
    }
 
    SECTION("Multi-segment")
@@ -176,7 +176,137 @@ TEST_CASE("PWLinearPSD integration", "[PSD]")
       CAPTURE(kA, kB);
       REQUIRE_THAT(psd.Integrate(kA, kB), WithinULP(exact, 5));
    }
-   
+}
+
+TEST_CASE("PWLogLogPSD integration", "[PSD]")
+{
+   SECTION("Single segment")
+   {
+      constexpr std::array<double,2> kX{5e3, 20e3}, 
+                                     kY{1e-8, 1e-9};
+      const double kM = std::log10(kY[1]/kY[0])/std::log10(kX[1]/kX[0]);
+
+      // Ensure that kM != -1
+      REQUIRE(kM != -1);
+
+      const double kA=GENERATE(2.5e3,5e3,10e3); 
+      const double kB=GENERATE(15e3, 20e3, 25e3);
+      
+      // Definite integral of y0*(x/x0)^m computed via Wolfram Alpha
+      const double kExact = (kY[0]/(kM+1))*(kB*std::pow(kB/kX[0], kM)
+                                             -kA*std::pow(kA/kX[0], kM));
+
+      PWLogLogPSD psd(kX,kY);
+      CAPTURE(kA, kB);
+      CAPTURE(psd.Integrate(kA, kB), kExact);
+      REQUIRE_THAT(psd.Integrate(kA, kB), WithinULP(kExact,5));
+   }
+
+   SECTION("Single segment, m=-1")
+   {
+      constexpr std::array<double,2> kX{1e3, 10e3}, 
+                                     kY{1e-6, 1e-7};
+      const double kM = std::log10(kY[1]/kY[0])/std::log10(kX[1]/kX[0]);
+
+      // Ensure that kM == -1
+      REQUIRE(kM == -1);
+
+      const double kA=GENERATE(0.5e3,1e3,2.5e3); 
+      const double kB=GENERATE(7.5e3, 10e3, 15e3);
+      
+      // Definite integral of y0*(x/x0)^(-1) computed via Wolfram Alpha
+      const double kExact = kX[0]*kY[0]*(std::log(kB)-std::log(kA));
+
+      PWLogLogPSD psd(kX,kY);
+      CAPTURE(kA, kB);
+      CAPTURE(psd.Integrate(kA, kB), kExact);
+      REQUIRE_THAT(psd.Integrate(kA, kB), WithinULP(kExact,5));
+   }
+
+   // SECTION("Multi-segment")
+   // {
+   //    constexpr std::array<double,4> kX{1.0, 3.0,  5.0,  7.0}, 
+   //                                   kY{2.0, 4.0, -6.0, -8.0};
+   //    constexpr std::size_t kNumIv = kX.size() -1;
+   //    constexpr std::array<double,kNumIv> kM = 
+   //    [&]()
+   //    {
+   //       std::array<double, kNumIv> slopes;
+   //       for (std::size_t i = 0; i < kNumIv; i++)
+   //       {
+   //          slopes[i] = (kY[i+1]-kY[i])/(kX[i+1]-kX[i]);
+   //       }
+   //       return slopes;
+   //    }();
+
+   //    // Define function pointers of integral for each interval
+   //    const std::array<std::function<double(double,double)>,kNumIv> kExact =
+   //    [&]()
+   //    {
+   //       std::array<std::function<double(double,double)>,kNumIv> exact_funcs;
+   //       for (std::size_t i = 0; i < kNumIv; i++)
+   //       {
+   //          exact_funcs[i] = 
+   //          [&,i](double a, double b) -> double
+   //          {
+   //             return 0.5*(b-a)*(kM[i]*(a+b-2*kX[i])+2*kY[i]);
+   //          };
+   //       }
+   //       return exact_funcs;
+   //    }();
+
+   //    const double kA=GENERATE(0.5, 1.0, 1.5, 3.0, 3.3); 
+   //    const double kB=GENERATE(3.5, 5.0, 5.5, 7.0, 8.0);
+
+   //    if (kA > kB)
+   //    {
+   //       FAIL("Error in test configuration. kA cannot be > kB.");
+   //    }
+
+   //    double exact = 0.0;
+   //    for (std::size_t i = 0; i < kNumIv; i++)
+   //    {
+   //       double left_bound = 0.0;
+   //       double right_bound = 0.0;
+
+   //       // End if we have finished integration.
+   //       if (kB < kX[i])
+   //       {
+   //          break;
+   //       }
+   //       // If a is before/at interval start AND not ahead of first interval
+   //       if (kA <= kX[i] && i != 0)
+   //       {
+   //          left_bound = kX[i];
+   //       }
+   //       // Else if a is within interval OR ahead of first interval
+   //       else if (kA < kX[i+1])
+   //       {
+   //          left_bound = kA;
+   //       }
+   //       // else not yet at interval of integration start
+   //       else
+   //       {
+   //          continue;
+   //       }
+
+   //       // If b is after/at interval end AND not behind final interval
+   //       if (kB >= kX[i+1] && i+1 != kNumIv)
+   //       {
+   //          right_bound = kX[i+1];
+   //       }
+   //       // Else must be within interval
+   //       else
+   //       {
+   //          right_bound = kB;
+   //       }
+   //       exact += kExact[i](left_bound, right_bound);
+   //    }
+      
+   //    PWLinearPSD psd(kX,kY);
+   //    CAPTURE(kA, kB);
+   //    REQUIRE_THAT(psd.Integrate(kA, kB), WithinULP(exact, 5));
+   // }
 }
 
 } // namespace jabber_test
