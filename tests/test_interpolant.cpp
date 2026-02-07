4abbe24547ee0ea48e7a8beea3cb35fed54ee71c
Joseph Signorelli <jms26@illinois.edu>
2026-02-07 13:36:07 -0600

    Improve interpolant testing via exact function, verified to exact precision

--- tests/test_interpolant.cpp
+++ tests/test_interpolant.cpp
@@ -6,6 +6,7 @@
 #include <cmath>
 
 using namespace jabber;
+using namespace Catch::Matchers;
 
 namespace jabber_test
 {
@@ -14,47 +15,88 @@ namespace jabber_test
 static constexpr std::array<double, 4> kX={0.1e3, 1e3, 10e3, 100e3};
 
 // Discrete y for interpolation.
-static constexpr std::array<double, 4> kY={1e-8, 5e-8, 7e-9, 2e-9};
+static constexpr std::array<double, 4> kY={1e-8, 5e-8, 7e-9, 2e-14};
 
 // Samples to evaluate at.
 static constexpr std::array<double, 6> kXSample={0.05e3, 0.7e3, 5e3, 10e3,
                                                  50e3, 150e3};
 
+// Piecewise interval START to compute in of kX/kY of kXSamples.
+static constexpr std::array<std::size_t, 6> kXInterval={0, 0, 1, 2, 2, 2};
+
+
+double ExactLinear(double x0,double y0,
+                   double x1, double y1, double x)
+{
+   const double m = (y1-y0)/(x1-x0);
+   return m*(x-x0) + y0;
+}
+
+double ExactLogLog(double x0, double y0, 
+                     double x1, double y1, double x)
+{
+   const double m = log10l(y1/y0)/log10l(x1/x0);
+   return y0*powl(x/x0, m);
+}
+
+TEST_CASE("Evaluation function verification", "[Interpolant]")
+{
+   SECTION("Linear")
+   {
+      REQUIRE_THAT(ExactLinear(1e1, 1e-8, 1e3, 1e-12, 1e2), WithinULP(9.091e-9, 0));
+   }
+
+   SECTION("LogLog")
+   {
+      REQUIRE_THAT(ExactLogLog(1e1, 1e-8, 1e3, 1e-12, 1e2), WithinULP(1e-10, 0));
+   }
+}
+
 TEST_CASE("PWLinear", "[Interpolant]")
 {
+   const std::array<double, kXSample.size()> kYSample=
+   []()
+   {
+      std::array<double, kXSample.size()> exact_y;
+      for (std::size_t i = 0; i < kXSample.size(); i++)
+      {
+         const std::size_t idx = kXInterval[i];
+         exact_y[i] = ExactLinear(kX[idx], kY[idx], 
+                                  kX[idx+1], kY[idx+1], kXSample[i]);
+      }
+      return exact_y;
+   }();
+
    PWLinear interp(kX, kY);
-   constexpr std::array<double, kXSample.size()> kYSample=
-      {(5e-8-1e-8)/(1e3-0.1e3)*(0.05e3-0.1e3) + 1e-8,
-       (5e-8-1e-8)/(1e3-0.1e3)*(0.7e3-0.1e3) + 1e-8,
-       (7e-9-5e-8)/(10e3-1e3)*(5e3-1e3) + 5e-8,
-       7e-9,
-       (2e-9-7e-9)/(100e3-10e3)*(50e3-10e3) + 7e-9,
-       (2e-9-7e-9)/(100e3-10e3)*(150e3-10e3) + 7e-9};
    
-   std::array<double, kXSample.size()> y_sample_interp;
    for (std::size_t i = 0; i < kXSample.size(); i++)
    {
       CAPTURE(i);
-      CHECK(interp(kXSample[i]) == kYSample[i]);
+      CHECK_THAT(interp(kXSample[i]), WithinULP(kYSample[i], 1));
    }
 }
 
 TEST_CASE("PWLogLog", "[Interpolant]")
 {
-   PWLogLog interp(kX, kY);
    const std::array<double, kXSample.size()> kYSample=
-      {1e-8*std::pow((0.05e3/0.1e3), std::log10(5e-8/1e-8)/std::log10(1e3/0.1e3)),
-       1e-8*std::pow((0.7e3/0.1e3), std::log10(5e-8/1e-8)/std::log10(1e3/0.1e3)),
-       5e-8*std::pow((5e3/1e3), std::log10(7e-9/5e-8)/std::log10(10e3/1e3)),
-       7e-9,
-       7e-9*std::pow((50e3/10e3), std::log10(2e-9/7e-9)/std::log10(100e3/10e3)),
-       7e-9*std::pow((150e3/10e3), std::log10(2e-9/7e-9)/std::log10(100e3/10e3))};
-   
-   std::array<double, kXSample.size()> y_sample_interp;
+   []()
+   {
+      std::array<double, kXSample.size()> exact_y;
+      for (std::size_t i = 0; i < kXSample.size(); i++)
+      {
+         const std::size_t idx = kXInterval[i];
+         exact_y[i] = ExactLogLog(kX[idx], kY[idx], 
+                                  kX[idx+1], kY[idx+1], kXSample[i]);
+      }
+      return exact_y;
+   }();
+
+   PWLogLog interp(kX, kY);
+
    for (std::size_t i = 0; i < kXSample.size(); i++)
    {
       CAPTURE(i);
-      CHECK(interp(kXSample[i]) == kYSample[i]);
+      CHECK_THAT(interp(kXSample[i]), WithinULP(kYSample[i], 1));
    }
 }
 
