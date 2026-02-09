60e8b9441b874dae14d8d656be708b28524eb1a3
Joseph Signorelli <jms26@illinois.edu>
2026-02-09 16:54:53 -0600

    Implement custom OptionGenerator

--- tests/test_utils.hpp
+++ tests/test_utils.hpp
@@ -1,6 +1,8 @@
 #ifndef JABBER_TEST_UTILS
 #define JABBER_TEST_UTILS
 
+#include <catch2/generators/catch_generators.hpp>
+
 #ifdef JABBER_WITH_APP
 #include <jabber_app.hpp>
 #endif // JABBER_WITH_APP
@@ -43,6 +45,47 @@ std::vector<double> GenerateRandomVec(int seed, double s0, double s1)
 {
    std::array<double, Size> temp = GenerateRandomArr<Size>(seed, s0, s1);
    return std::vector<double>(temp.begin(), temp.end());
+}
+
+/// Concept for enums of type std::uint8_t with a ::Size.
+template<typename T>
+concept OptionEnum = 
+         std::is_enum_v<T> && 
+         std::same_as<std::underlying_type_t<T>, std::uint8_t> &&
+         requires {T::Size; };
+
+/// Custom generator for \ref OptionEnum.
+template<OptionEnum T>
+class OptionGenerator : public Catch::Generators::IGenerator<T>
+{
+private:
+   T enumerator_val_;
+   std::uint8_t enum_idx_ = 0;
+public:
+   bool next() override
+   {
+      if (++enum_idx_ < static_cast<std::uint8_t>(T::Size))
+      {
+         enumerator_val_ = static_cast<T>(enum_idx_);
+         return true;
+      }
+      else
+      {
+         return false;
+      }
+   };
+
+   T const& get() const override
+   {
+      return enumerator_val_;
+   }
+};
+
+template <OptionEnum T>
+Catch::Generators::GeneratorWrapper<T> options()
+{
+    return Catch::Generators::GeneratorWrapper<T>(
+               Catch::Detail::make_unique<OptionGenerator<T>>());
 }
 
 #ifdef JABBER_WITH_APP
