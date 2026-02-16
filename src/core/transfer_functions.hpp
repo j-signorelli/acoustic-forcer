1c92e9a9b6d85cb8958af2d0193c67ded75969e9
Joseph Signorelli <jms26@illinois.edu>
2026-02-16 16:56:15 -0600

    Add low frequency limit analytical transfer function w/ unit test to core

--- src/core/transfer_functions.hpp
+++ src/core/transfer_functions.hpp
@@ -4,6 +4,17 @@
 namespace jabber
 {
 
+/**
+ * @brief Compute the transfer function $\chi$ using the analytical 
+ * low-frequency limit form in Chaudhry and Chandler, 2017.
+ * 
+ * @param mach_bar      Freestream Mach number.
+ * @param gamma         Specific heat ratio.
+ * @param speed         Wave speed, 'S' for slow, 'F' for fast.
+ * @return double       $\chi$.
+ */
+double LowFrequencyLimitTF(double mach_bar, double gamma, char speed);
+
 } // namespace jabber
 
 #endif // JABBER_TRANSFER_FUNCTIONS
