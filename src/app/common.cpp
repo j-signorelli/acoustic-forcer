3e4e807acde02c716fba5fb3486e104d4236bcb3
Joseph Signorelli <jms26@illinois.edu>
2026-03-03 17:25:22 -0600

    add missing deduction guide for overloads

--- src/app/common.cpp
+++ src/app/common.cpp
@@ -15,6 +15,10 @@ using namespace jabber;
 template<class... Ts>
 struct overloads : Ts... { using Ts::operator()...; };
 
+#if __cpp_deduction_guides < 201907L
+template<class... Ts>
+overloads(Ts...) -> overloads<Ts...>;
+#endif
 
 namespace jabber_app
 {
