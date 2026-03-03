2efab6a13b78012ef3f34671df1d470e59c83ab4
Joseph Signorelli <jms26@illinois.edu>
2026-03-03 12:56:13 -0600

    utils --> test_utils

--- tests/test_app_utils.hpp
+++ tests/test_app_utils.hpp
@@ -2,7 +2,7 @@
 #define JABBER_TEST_APP_UTILS
 
 #ifdef JABBER_WITH_APP
-#include "utils.hpp"
+#include "test_utils.hpp"
 
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
--- tests/test_psd.cpp
+++ tests/test_psd.cpp
@@ -1,4 +1,4 @@
-#include "utils.hpp"
+#include "test_utils.hpp"
 
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
